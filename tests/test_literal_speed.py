0a03f35dfe960f826ddc403f3856306457d979ca
Mike C. Fletcher <mcfletch@vrplumber.com>
2011-02-23 22:45:07 -0500

    Just with that the difference goes from 10x to 3.25x the speed for pypy

--- tests/test_literal_speed.py
+++ tests/test_literal_speed.py
@@ -1,9 +1,9 @@
 from simpleparse.objectgenerator import Literal
 
 def main():
-    a = Literal( 'a', repeating=True )
+    a = Literal( 'a', repeating=True, optional=True, negative=True )
     ap = a.to_parser()
-    print ap( 'a' * 1024 * 1024 * 2 )
+    print ap( 'b' * 1024 * 1024 * 2 )
 
 if __name__ == "__main__":
     main()
