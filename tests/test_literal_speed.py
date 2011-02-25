e326f540e17abd6ececceff1901e9e808e641c6b
Mike C. Fletcher <mcfletch@vrplumber.com>
2011-02-24 22:43:06 -0500

    Do actual sequence rather than a single child, provide some variation in the things parsed.

--- tests/test_literal_speed.py
+++ tests/test_literal_speed.py
@@ -7,7 +7,7 @@ def main():
     n = Name( 'a', repeating=True )
     n._target = b.final_method()
     ap = n.to_parser()
-    print ap( 'a' * 1024 * 1024 )[-1]
+    print ap( 'abacaa' * 1024 * 256 )[-1]
 
 if __name__ == "__main__":
     main()
