f966bdccc3bb66b20dbfdceda507cc481c800d2e
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-07-01 02:00:52 -0400

    Comment out usage of the cython object generator, as it is slower than the pure-python version (duh!)

--- objectgenerator.py
+++ objectgenerator.py
@@ -1,5 +1,5 @@
 """Abstraction point to load either the pure-python or Cython parser implementation"""
 #try:
-from simpleparse._objectgenerator import *
+#    from simpleparse._objectgenerator import *
 #except ImportError, err:
-#    from simpleparse._pureobjectgenerator import *
+from simpleparse._pureobjectgenerator import *
